54e82285ba16101b2eefd149d3cf91d6f01ebd21
rald <thecoconutnutisagiantnut@gmail.com>
2026-08-26 07:47:28 +0800

    OK

---
+++ x11gfx-05-OK/archives/main-04-OK-hit areas.c
@@ -0,0 +1,74 @@
+#define GL2D_IMPLEMENTATION
+#include "gl2d.h"
+
+int main() {
+    FastCanvas canvas;
+
+    if (!gl2d_init(&canvas)) {
+        return 1;
+    }
+
+    MouseState mouse = {0};
+
+    int ball_x = 100, ball_y = 100;
+    int vel_x = 15, vel_y = 10;
+    int radius = 30;
+
+    int rect_x = 200, rect_y = 150, rect_w = 100, rect_h = 60;
+    int circle_cx = 450, circle_cy = 180, circle_r = 40;
+    
+    int rect_clicked = 0;
+    int circle_clicked = 0;
+
+    int running = 1;
+    while (running) {
+        gl2d_poll_events(&canvas, &mouse);
+
+        // Define multiple input regions so both the rectangle and circle catch mouse clicks
+        XRectangle interactive_areas[] = {
+            { (short)rect_x, (short)rect_y, (unsigned short)rect_w, (unsigned short)rect_h },
+            { (short)(circle_cx - circle_r), (short)(circle_cy - circle_r), (unsigned short)(circle_r * 2), (unsigned short)(circle_r * 2) }
+        };
+
+        // Enable both interactive regions simultaneously
+        gl2d_set_input_regions(&canvas, interactive_areas, 2, 1);
+
+        if (mouse.left_button) {
+            if (gl2d_inrect(mouse.x, mouse.y, rect_x, rect_y, rect_w, rect_h)) {
+                rect_clicked = 1;
+            }
+
+            if (gl2d_incirc(mouse.x, mouse.y, circle_cx, circle_cy, circle_r)) {
+                circle_clicked = 1;
+            }
+        } else {
+            rect_clicked = 0;
+            circle_clicked = 0;
+        }
+
+        fast_cls(&canvas, 0x00000000);
+
+        ball_x += vel_x;
+        ball_y += vel_y;
+        if (ball_x - radius < 0 || ball_x + radius > canvas.width) vel_x = -vel_x;
+        if (ball_y - radius < 0 || ball_y + radius > canvas.height) vel_y = -vel_y;
+
+        fast_filled_circle(&canvas, ball_x, ball_y, radius, 0xFF000080); 
+        fast_circle(&canvas, ball_x, ball_y, radius, 0xFFFFFFFF);       
+        fast_line(&canvas, canvas.width/2, canvas.height/2, ball_x, ball_y, 0xFFFF0000);
+
+        unsigned long rect_color = rect_clicked ? 0xFFFF00FF : 0xFF00FF00; 
+        fast_filled_rect(&canvas, rect_x, rect_y, rect_w, rect_h, rect_color);       
+        fast_rect(&canvas, rect_x, rect_y, rect_w, rect_h, 0xFFFFFFFF);             
+
+        unsigned long circle_color = circle_clicked ? 0xFFFF00FF : 0xFF00FF00;
+        fast_filled_circle(&canvas, circle_cx, circle_cy, circle_r, circle_color);
+        fast_circle(&canvas, circle_cx, circle_cy, circle_r, 0xFFFFFFFF);
+
+        gl2d_update(&canvas);
+        usleep(16000); // ~60 FPS
+    }
+
+    gl2d_quit(&canvas);
+    return 0;
+}
---
+++ x11gfx-05-OK/test_font.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+#include "font.h"
+
+#define GL2D_IMPLEMENTATION
+#include "gl2d.h"
+
+char *my_strupr(char *s) {
+    char *p = s;
+    while (*p) {
+        *p = toupper((unsigned char)*p);
+        p++;
+    }
+    return s;
+}
+
+void dchar(FastCanvas *fc,char ch,int x,int y,unsigned long  color,int size) {
+  for(int k=0;k<45;k++) {
+    if(font[k][0]==ch) {
+      for(int j=0;j<5;j++) {
+        for(int i=0;i<3;i++) {
+          if(font[k][j*3+i+1]==1) {
+            frect(fc,i*size+x,j*size+y,size,size,color);
+          }
+        }
+      }
+      break;
+    }
+  }
+}
+
+void dtext(FastCanvas *fc,char *t,int x,int y,unsigned long color,int size) {
+  for(int k=0;t[k];k++) {
+    dchar(fc,t[k],x,y,color,size);
+    x+=4*size;
+    if(x+4*size>fc->width) {
+      x=0;
+      y+=6*size;
+    }
+  }
+}
+
+int main() {
+
+time_t rawtime;
+    struct tm *info;
+    char buffer[80];
+
+    FastCanvas canvas;
+
+    if (!gl2d_init(&canvas)) {
+        return 1;
+    }
+
+    MouseState mouse = {0};
+
+
+    int running = 1;
+    while (running) {
+        gl2d_poll_events(&canvas, &mouse);
+
+        cls(&canvas, 0x00000000);
+
+        time(&rawtime);
+        info = localtime(&rawtime);
+        strftime(buffer, sizeof(buffer), "%A, %B %d, %Y %l:%M:%S %p", info);
+
+        my_strupr(buffer);
+
+        dtext(&canvas,buffer,
+            canvas.width-strlen(buffer)*4*2,
+            0,0x40FFFFFF,2
+        );
+
+        gl2d_update(&canvas);
+        usleep(16000); // ~60 FPS
+    }
+
+    gl2d_quit(&canvas);
+    return 0;
+}
